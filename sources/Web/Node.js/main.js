1ce5d050a20df318d974077da57cd9c5b1f702d4
excodaw <dhtpgkr1999@gmail.com>
2023-02-06 15:18:26 +0900

    node.js 짧게 공부

--- sources/Web/Node.js/main.js
+++ sources/Web/Node.js/main.js
@@ -6,36 +6,78 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      TITLE = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/') {
+      if(queryData.id === undefined) {
+        fs.readdir('./data', function(error, filelist){
+          var title = `Welcome`;
+          var description = `Hello`
+          var list = '<ul>';
+
+          var i = 0;
+          while(i < filelist.length) {
+            list = list + `<li><a href = "/?id = ${filelist[i]}">${filelist[i]}</li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);  
+          response.end(template);
+        });
+      }
+      else {  //id 값이 있는 경우
+        fs.readdir('./data', function(error, filelist){
+          var title = `Welcome`;
+          var description = `Hello`
+          var list = '<ul>';
+
+          var i = 0;
+          while(i < filelist.length) {
+            list = list + `<li><a href = "/?id = ${filelist[i]}">${filelist[i]}</li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>';
+          fs.readFile(`data/${queryData.id}.txt`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200); 
+            response.end(template);
+          });
+        });
+      }
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
+    else {
+      response.writeHead(404); // 파일을 찾을 수 없다는 뜻
+      response.end('Not found');
     }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}.txt`, 'utf8', function(err, description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="index.html">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    });
 });
 app.listen(3000);
