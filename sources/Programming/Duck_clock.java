264e712be1574ea2330003c74508577d67cc606e
sehako <dhtpgkr1999@gmail.com>
2023-05-11 13:57:53 +0900

    백준 풀이 업로드

--- sources/Programming/Duck_clock.java
+++ sources/Programming/Duck_clock.java
@@ -13,31 +13,12 @@ class Duck_clock {
         int num = sc.nextInt();
         sc.close();
 
-        while(num >= 60) {
-            num -= 60;
-            num_m++;
-        }
-
-        while(num_m >= 60) {
-            num_m -= 60;
-            num_h++;
-        }
-
-        h += num_h;
-        m += num_m;
         s += num;
-
-        if(s >= 60) {
-            m++;
-            s -= 60;
-        }
-        if(m >= 60) {
-            h++;
-            m -= 60;
-        }
-        if(h >= 24) {
-            h -= 24;
-        }
+        m += s / 60;
+        s %= 60;
+        h += m / 60;
+        m %= 60;
+        h %= 24;
 
         System.out.print(h + " " + m + " " + s);
     }
