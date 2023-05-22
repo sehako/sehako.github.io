a5a1268fabeb51100ff3ddd7097facb1e20a8ea8
excodaw <dhtpgkr1999@gmail.com>
2023-05-22 17:50:29 +0900

    소스 코드 전부 삭제

--- sources/Programming/Sample.java
+++
@@ -1,7 +0,0 @@
-package sources.Programming;
-
-class Sample {
-    public static void main(String[] args) {
-
-    }
-}
--- sources/Web/Node.js/Node/fileread.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs') //파일 시스템을 불러오는 모듈(?)
-
-fs.readFile('sample.txt', 'utf8', function(err, data) {
-    console.log(data);
-});
