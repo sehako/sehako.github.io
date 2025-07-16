382fe52fc4284d0af9b251613d1343481662e0f2
sehako <dhtpgkr1999@gmail.com>
2025-07-16 17:43:20 +0900

    코드 복사 기능 테스트_2

--- assets/js/code-copy.js
+++ assets/js/code-copy.js
@@ -1,50 +1,24 @@
-// const codeBlocks = document.querySelectorAll(
-//   ".code-header + .highlighter-rouge"
-// );
-// const copyCodeButtons = document.querySelectorAll(".copy-code-button");
-
-// copyCodeButtons.forEach((copyCodeButton, index) => {
-//   const code = codeBlocks[index].innerText;
-//   let id;
-
-//   copyCodeButton.addEventListener("click", () => {
-//     window.navigator.clipboard.writeText(code);
-
-//     const img = copyCodeButton.querySelector("img");
-//     img.src = "/assets/images/check.png";
-
-//     if (id) {
-//       clearTimeout(id);
-//     }
-
-//     id = setTimeout(() => {
-//       img.src = "/assets/images/copy.png";
-//     }, 2000);
-//   });
-// });
-
-// This assumes that you're using Rouge; if not, update the selector
-const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+const codeBlocks = document.querySelectorAll(
+  ".code-header + .highlighter-rouge"
+);
+const copyCodeButtons = document.querySelectorAll(".copy-code-button");
 
 copyCodeButtons.forEach((copyCodeButton, index) => {
   const code = codeBlocks[index].innerText;
+  let id;
 
-  copyCodeButton.addEventListener('click', () => {
-    // Copy the code to the user's clipboard
+  copyCodeButton.addEventListener("click", () => {
     window.navigator.clipboard.writeText(code);
 
-    // Update the button text visually
-    const { innerText: originalText } = copyCodeButton;
-    copyCodeButton.innerText = 'Copied!';
+    const img = copyCodeButton.querySelector("img");
+    img.src = "/assets/images/check.svg";
 
-    // (Optional) Toggle a class for styling the button
-    copyCodeButton.classList.add('copied');
+    if (id) {
+      clearTimeout(id);
+    }
 
-    // After 2 seconds, reset the button to its initial UI
-    setTimeout(() => {
-      copyCodeButton.innerText = originalText;
-      copyCodeButton.classList.remove('copied');
+    id = setTimeout(() => {
+      img.src = "/assets/images/copy.svg";
     }, 2000);
   });
 });
